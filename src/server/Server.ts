04e79b31cf626c36fba00b2d63158dcae4f9c699
igorcol <igor.colombini@gmail.com>
2025-02-26 14:34:54 -0300

    initial config

---
+++ src/server/Server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+
+const server = express();
+
+// Configurações do servidor
+server.get('/', (_, res) => {
+    return res.send("Olá dev")
+});
+
+
+export { server }
