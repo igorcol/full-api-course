7aa165a76e0989c59c1ac8b28608394aa97d1411
igorcol <igor.colombini@gmail.com>
2025-02-27 18:07:16 -0300

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { server } from "./server/Server";
 
 // Env variables
 const PORT = process.env.PORT || 3030
+const ENV = process.env.NODE_ENV
 
 server.listen(PORT, () => {
     console.log(`
     ===========================
     ✅ API ONLINE           
-    ⚡ Port: ${PORT}        
+    ⚡ Port: ${PORT}          
+    ⚡ Env: ${ENV}      
     ===========================
     `);
 })
