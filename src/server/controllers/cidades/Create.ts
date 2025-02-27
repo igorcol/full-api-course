be9ce14fb85e97eae7839df04027d3f6a68efb89
igorcol <igor.colombini@gmail.com>
2025-02-27 01:47:30 -0300

    minor changes

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,6 +3,7 @@ import { Request, Response } from "express"
 import * as yup from 'yup'
 
 import { validation } from "../../shared/middleware";
+import { StatusCodes } from "http-status-codes";
 
 // * CIDADE
 // Schema do Body
@@ -16,27 +17,16 @@ const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
     estado: yup.string().required().max(2).min(2)
 })
 
-// * FILTER
-// Schema do Filter
-interface IFilter {
-    filter?: string;
-}
-// Schema da Validação do Filter
-const fiterValidation: yup.ObjectSchema<IFilter> = yup.object().shape({
-    filter: yup.string().optional()
-})
-
 
 //* Validação para todas as propriedades da controller
 export const createValidation = validation((getSchema) => ({
-    body: getSchema(bodyValidation),
-    query: getSchema(fiterValidation)
+    body: getSchema(bodyValidation)
 }));
 
 
 //* Criar Cidade
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
     const DATA = req.body
-    console.log('POST | Cidade Criada:', DATA)
-    return res.send(DATA)
+    console.log('POST | Cidade:', DATA)
+    return res.status(StatusCodes.NOT_IMPLEMENTED).send('NÃO IMPLEMENTADO')
 }
