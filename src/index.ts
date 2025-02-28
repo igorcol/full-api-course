895227eaa4ef3d213496809331ebca0211bafd23
igorcol <igor.colombini@gmail.com>
2025-02-28 02:25:39 -0300

    feature(src/index): Migrate before launching to production

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";
 
 // Env variables
 const PORT = process.env.PORT || 3030
 const ENV = process.env.NODE_ENV
 
-server.listen(PORT, () => {
-    console.log(`
-    ===========================
-    ✅ API ONLINE           
-    ⚡ Port: ${PORT}          
-    ⚡ Env: ${ENV}      
-    ===========================
-    `);
-})
+const startServer = () => {
+    server.listen(PORT, () => {
+        console.log(`
+        ===========================
+        ✅ API ONLINE           
+        ⚡ Port: ${PORT}          
+        ⚡ Env: ${ENV}      
+        ===========================
+        `);
+    })
+}
+
+// Faz as migrations antes de iniciar o server em produção
+if (process.env.IS_LOCALHOST !== 'true') {
+    Knex.migrate.latest().then(() => {
+        startServer();
+    })
+    .catch(console.log);
+}
+else {
+    startServer();
+}
